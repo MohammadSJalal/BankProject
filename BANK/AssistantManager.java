4c33b3f9b1c43fb1bf1cbc670d77005dd150e0a0
sourena <113454178+sizhawastaken@users.noreply.github.com>
2025-06-13 22:21:52 +0330

    Update AssistantManager.java

--- BANK/AssistantManager.java
+++ BANK/AssistantManager.java
@@ -1,50 +1,62 @@
 package BANK;
 
-public class AssistantManager extends Employee {
-    private static int counter = 0;
-    private double salary;
+public class AssistantManager extends Employee { private static int counter = 0; private double salary;
 
-    public AssistantManager(Branch branchWork) {
-        this.branchWork = branchWork;
-        this.employeeIdentity = "A" + branchWork.getId() + counter++;
-        setSalary();
-        branchWork.addEmployee(this);
-    }
+public AssistantManager(Branch branchWork) {
+    this.branchWork = branchWork;
+    this.employeeIdentity = "A" + branchWork.getId() + counter++;
+    setSalary();
+    branchWork.addEmployee(this);
+}
 
-    @Override
-    public void setSalary() {
-        this.salary = 1.5 * BaseSalary;
-    }
+@Override
+public void setSalary() {
+    this.salary = 1.5 * BaseSalary;
+}
 
-    public double getSalary() {
-        return salary;
-    }
+public double getSalary() {
+    return salary;
+}
 
-    public void agreeWithRequest(String requestType, Customer customer) {
-        if (requestType.equalsIgnoreCase("loan")) {
-            boolean hasLoan = false;
-            if (hasLoan) {
-                customer.addMessage("درخواست وام رد شد: وام فعال دارید.");
-            } else {
-                BranchManager manager = getBranchManager();
-                if (manager != null) {
-                    manager.receiveMessage("تایید نهایی وام برای مشتری: " + customer.getCustomerId());
-                }
-            }
+public void agreeWithRequest(String requestType, Customer customer) {
+    if (requestType.equalsIgnoreCase("loan")) {
+        if (customer.hasActiveLoan()) {
+            customer.addMessage("درخواست وام رد شد: شما دارای وام فعال هستید.");
+            return;
         }
-    }
-
-    private BranchManager getBranchManager() {
-        for (Employee e : branchWork.getEmployees()) {
-            if (e instanceof BranchManager) {
-                return (BranchManager) e;
-            }
+        BranchManager manager = getBranchManager();
+        if (manager != null) {
+            manager.receiveMessage("تایید نهایی وام برای مشتری: " + customer.getCustomerId());
+            Request req = new Request("Loan Final Approval", customer);
+            customer.getBank().addRequest(req);
+        } else {
+            customer.addMessage("مدیر شعبه یافت نشد. درخواست تایید نشد.");
         }
-        return null;
-    }
-
-    @Override
-    public String toString() {
-        return "Assistant Manager [ID=" + employeeIdentity + ", Salary=" + salary + ", Branch=" + branchWork.getId() + "]";
+    } else if (requestType.equalsIgnoreCase("close account")) {
+        BranchManager manager = getBranchManager();
+        if (manager != null) {
+            manager.receiveMessage("تایید نهایی حذف حساب مشتری: " + customer.getCustomerId());
+            Request req = new Request("Close Account Final Approval", customer);
+            customer.getBank().addRequest(req);
+        } else {
+            customer.addMessage("مدیر شعبه یافت نشد. درخواست حذف حساب تایید نشد.");
+        }
     }
 }
+
+private BranchManager getBranchManager() {
+    for (Employee e : branchWork.getEmployees()) {
+        if (e instanceof BranchManager) {
+            return (BranchManager) e;
+        }
+    }
+    return null;
+}
+
+@Override
+public String toString() {
+    return "Assistant Manager [ID=" + employeeIdentity + ", Salary=" + salary + ", Branch=" + branchWork.getId() + "]";
+}
+
+}
+
