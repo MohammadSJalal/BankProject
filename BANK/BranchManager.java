995e4d9a23609363783306901a1fadc2eed942c1
sourena <113454178+sizhawastaken@users.noreply.github.com>
2025-06-13 04:17:31 +0330

    Update BranchManager.java

--- BANK/BranchManager.java
+++ BANK/BranchManager.java
@@ -1,30 +1,31 @@
 package BANK;
+
 public class BranchManager extends Employee {
-    public static int counter = 0;
+    private static int counter = 0;
     private double salary;
+
     public BranchManager(Branch branchWork) {
-        super();
         this.branchWork = branchWork;
-        this.employeeIdentity = "M"+ branchWork.getId() + counter;
-        counter++;
+        this.employeeIdentity = "M" + branchWork.getId() + counter++;
+        setSalary();
+        branchWork.addEmployee(this);
     }
+
     @Override
-    public void setSalary(){
+    public void setSalary() {
         this.salary = 2 * BaseSalary;
     }
-    public void closeAccountCostumer(){
 
+    public double getSalary() {
+        return salary;
     }
-    public void ShowAllEmployees(){
-        for (Employee i : branchWork.getEmployees()){
-            System.out.println(i);
-        }
+
+    public void agreeWithRequest(String message) {
+        System.out.println("مدیر تایید کرد: " + message);
     }
+
     @Override
-    public String toString(){
-        return "level Branch Manager\nemployee id : " + this.employeeIdentity + "\nsalary : " + this.salary + "\nbranch work id : " + this.branchWork.toString();
-    }
-    public void agreeWithRequest(){
-
+    public String toString() {
+        return "Branch Manager [ID=" + employeeIdentity + ", Salary=" + salary + ", Branch=" + branchWork.getId() + "]";
     }
 }
