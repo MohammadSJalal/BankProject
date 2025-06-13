d5f9c27abdfd7c731451cbb76a436d669c46e9f2
sourena <113454178+sizhawastaken@users.noreply.github.com>
2025-06-13 04:15:11 +0330

    Update AssistantManager.java

--- BANK/AssistantManager.java
+++ BANK/AssistantManager.java
@@ -1,50 +1,31 @@
 package BANK;
 
-public class AssistantManager extends Employee {
+public class BranchManager extends Employee {
     private static int counter = 0;
     private double salary;
 
-    public AssistantManager(Branch branchWork) {
+    public BranchManager(Branch branchWork) {
         this.branchWork = branchWork;
-        this.employeeIdentity = "A" + branchWork.getId() + counter++;
+        this.employeeIdentity = "M" + branchWork.getId() + counter++;
         setSalary();
         branchWork.addEmployee(this);
     }
 
     @Override
     public void setSalary() {
-        this.salary = 1.5 * BaseSalary;
+        this.salary = 2 * BaseSalary;
     }
 
     public double getSalary() {
         return salary;
     }
 
-    public void agreeWithRequest(String requestType, Customer customer) {
-        if (requestType.equalsIgnoreCase("loan")) {
-            boolean hasLoan = false;
-            if (hasLoan) {
-                customer.addMessage("درخواست وام رد شد: وام فعال دارید.");
-            } else {
-                BranchManager manager = getBranchManager();
-                if (manager != null) {
-                    manager.receiveMessage("تایید نهایی وام برای مشتری: " + customer.getCustomerId());
-                }
-            }
-        }
-    }
-
-    private BranchManager getBranchManager() {
-        for (Employee e : branchWork.getEmployees()) {
-            if (e instanceof BranchManager) {
-                return (BranchManager) e;
-            }
-        }
-        return null;
+    public void agreeWithRequest(String message) {
+        System.out.println("مدیر تایید کرد: " + message);
     }
 
     @Override
     public String toString() {
-        return "Assistant Manager [ID=" + employeeIdentity + ", Salary=" + salary + ", Branch=" + branchWork.getId() + "]";
+        return "Branch Manager [ID=" + employeeIdentity + ", Salary=" + salary + ", Branch=" + branchWork.getId() + "]";
     }
 }
