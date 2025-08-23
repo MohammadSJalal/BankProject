495c3a1ce5e486f052de0f3be619ffbb6f8ae33d
sourena <113454178+sizhawastaken@users.noreply.github.com>
2025-08-23 19:19:36 +0330

    Update Employee.java

--- BANK/Employee.java
+++ BANK/Employee.java
@@ -1,16 +1,12 @@
-package BANK;
-
 import java.util.ArrayList;
 import java.util.List;
 
-// کلاس پایه برای همه‌ی کارمندای بانک
 public abstract class Employee {
-    protected String employeeIdentity; // شناسه‌ی منحصربه‌فرد کارمند
-    protected final double BaseSalary = 1000.00; // حقوق پایه
-    protected Branch branchWork; // شعبه‌ای که کارمند توش فعاله
-    protected List<String> messages = new ArrayList<>(); // صندوق پیام کارمند
+    protected String employeeIdentity;
+    protected final double BaseSalary = 1000.00;
+    protected Branch branchWork;
+    protected List<String> messages = new ArrayList<>();
 
-    // متد انتزاعی برای تنظیم حقوق که باید تو کلاسای فرزند پیاده‌سازی بشه
     public abstract void setSalary();
 
     public void receiveMessage(String message) {
