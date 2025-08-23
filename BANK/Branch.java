8d67b2c2497c9843b832ede9a87dd948e1cafe0d
sourena <113454178+sizhawastaken@users.noreply.github.com>
2025-08-23 19:17:17 +0330

    Update Branch.java

--- BANK/Branch.java
+++ BANK/Branch.java
@@ -1,19 +1,24 @@
-package BANK;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Branch {
-    public static char counter = 'a';
-    public static StringBuilder lastId = new StringBuilder("");
+    private static char counter = 'a';
+    private static StringBuilder lastId = new StringBuilder("");
     private StringBuilder id = new StringBuilder("");
     protected String name;
     private BranchManager manager;
     private AssistantManager deputy;
-    protected Employee [] employees;
+    private List<Employee> employees = new ArrayList<>();
+
     public Branch() {
         this.id.append(addACharToLastId(lastId));
     }
-    public Branch(String name){
+
+    public Branch(String name) {
         this.name = name;
         this.id.append(addACharToLastId(lastId));
     }
+
     public static StringBuilder addACharToLastId(StringBuilder subId) {
         if (subId.length() == 0) {
             return subId.append('a');
@@ -22,29 +27,37 @@ public class Branch {
         subId.deleteCharAt(lastId.length() - 1);
         if (lastChar == 'z') {
             return addACharToLastId(subId).append("a");
-        }
-        else {
+        } else {
             lastChar++;
             subId.append(lastChar);
         }
         return subId;
     }
+
+    public void addEmployee(Employee e) {
+        employees.add(e);
+        if (e instanceof BranchManager) manager = (BranchManager) e;
+        if (e instanceof AssistantManager) deputy = (AssistantManager) e;
+    }
+
     public String getId() {
         return id.toString();
     }
+
     @Override
     public String toString() {
-        if (this.name.equals("")) {
+        if (this.name == null || this.name.equals("")) {
             return "branch id=" + id + ", manager=" + manager + ", deputy=" + deputy;
-        }
-        else {
-            return "name of branch " + name + " , branch id :"+ id +", manager=" + manager + ", deputy=" + deputy;
+        } else {
+            return "name of branch " + name + " , branch id :" + id + ", manager=" + manager + ", deputy=" + deputy;
         }
     }
+
     public void showInformation() {
         System.out.println(this);
     }
-    public Employee [] getEmployees() {
+
+    public List<Employee> getEmployees() {
         return employees;
     }
 }
